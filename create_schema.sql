6d0944921088559d08d8d376afa9a7280f82a1f4
Rexford Wiafe <w.rexford@yahoo.com>
2022-06-27 15:35:00 -0500

    intro to sql basics

--- create_schema.sql
+++ create_schema.sql
@@ -1,9 +1,33 @@
--- Schema
+
 DROP TABLE IF EXISTS students;
 CREATE TABLE students (
-  id           serial PRIMARY KEY,
-  first_name   varchar(255) NOT NULL,
-  last_name    varchar(255) NOT NULL,
-  birthdate    date NOT NULL,
-  address_id   integer
+  id  serial PRIMARY KEY,
+  first_name  varchar(50) NOT NULL,
+  last_name varchar(50) NOT NULL,
+  birthdate date NOT NULL,
+  addressId  integer
+);
+
+DROP TABLE IF EXISTS addresses;
+CREATE TABLE addresses (
+  id  serial PRIMARY KEY,
+  line_1  varchar(100) NOT NULL,
+  city  varchar(50) NOT NULL,
+  address_state varchar(100) NOT NULL,
+  zipcode varchar(10) NOT NULL
+);
+
+DROP TABLE IF EXISTS classes;
+CREATE TABLE classes (
+  id  serial PRIMARY KEY,
+  class_name  varchar(100) NOT NULL,
+  credits integer NOT NULL
+);
+
+DROP TABLE IF EXISTS enrollments;
+CREATE TABLE enrollments (
+  id serial PRIMARY KEY,
+  studentId integer NOT NULL,
+  classId integer NOT NULL,
+  grade character(2)
 );
